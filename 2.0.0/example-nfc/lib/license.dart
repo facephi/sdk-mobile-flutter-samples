3117c11ceb1af617dcb080d8adabd766ce8c8cb5
willicom2000 <willicom2000_9@hotmail.com>
2024-11-18 10:06:09 +0100

    [master] feat: upd examples for the latest version of Flutter.

--- 2.0.0/example-nfc/lib/license.dart
+++ 2.0.0/example-nfc/lib/license.dart
@@ -1,7 +1,7 @@
 const String SELPHI_RESOURCE_PATH = 'fphi-selphid-widget-resources-selphid-1.0';
 const String CUSTOMER_ID          = "flutter@facephi.com";
 
-const LICENSE_URL             = "https://license.identity-platform.pro";
+const LICENSE_URL             = "";
 const LICENSE_APIKEY_ANDROID  = "";
 const LICENSE_APIKEY_IOS      = "";
 
--- 2.0.0/example-videoid/lib/license.dart
+++ 2.0.0/example-videoid/lib/license.dart
@@ -1,9 +1,9 @@
 const String SELPHI_RESOURCE_PATH = 'fphi-selphid-widget-resources-selphid-1.0';
 const String CUSTOMER_ID          = "flutter@facephi.com";
 
-const LICENSE_URL             = "";
-const LICENSE_APIKEY_ANDROID  = "";
-const LICENSE_APIKEY_IOS      = "";
+const LICENSE_APIKEY_ANDROID     = "";
+const LICENSE_APIKEY_IOS         = "";
+const LICENSE_URL                = "";
 
 const LICENSE_ANDROID = "{}";
 const LICENSE_IOS     = "{}";
