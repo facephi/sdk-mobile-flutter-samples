687d26951417bdb9ac56f00d3d1b9f3839194269
willicom2000 <willicom2000_9@hotmail.com>
2025-10-22 10:32:26 +0200

    Update core_widget.dart

--- 2.3.0/example-nfc/lib/models/core_widget.dart
+++ 2.3.0/example-nfc/lib/models/core_widget.dart
@@ -69,7 +69,7 @@ class CoreWidget
     try
     {
       final Map m = await FphiSdkmobileCore().initFlow(widgetConfigurationJSON: FlowConfiguration(
-          mCustomerId: customerId, mFlow: "acc560f0-8cbc-475b-b479-1f22ae5cdae8", mPreview: false)
+          mCustomerId: customerId, mFlow: "acc560f0-8cbc-475b-b479-1f22ae5cdae8")
       );
       return Right(CoreResult.fromMap(m));
     }
