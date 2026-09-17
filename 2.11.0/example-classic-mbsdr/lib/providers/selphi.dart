e9405a1b7e004c4dc72f6d803b0a994c9ffd21ec
willicom2000 <willicom2000_9@hotmail.com>
2026-09-17 09:17:07 +0200

    Update selphi.dart

--- 2.11.0/example-classic-mbsdr/lib/providers/selphi.dart
+++ 2.11.0/example-classic-mbsdr/lib/providers/selphi.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:example/models/selphi_face_result.dart';
 import 'package:example/models/selphi_face_widget.dart';
 import 'package:flutter/foundation.dart';
-import 'package:fphi_sdkmobile_core/fphi_sdkmobile_core_error_type.dart';
 import 'package:fphi_sdkmobile_core/fphi_sdkmobile_core_finish_status.dart';
 
 void launchSelphiAuthenticate(void Function(VoidCallback fn) setState, ValueNotifier<String> message, ValueNotifier<Uint8List?> bestImage) async
@@ -19,7 +18,7 @@ void launchSelphiAuthenticate(void Function(VoidCallback fn) setState, ValueNoti
           bestImage.value = base64Decode(r.bestImage!);
           break;
         case SdkFinishStatus.STATUS_ERROR: // Error
-          message.value   = SdkErrorType.getDiagnosticError(r.errorDiagnostic);
+          message.value   = r.errorDiagnostic.replaceAll('_', ' ');
           bestImage.value = null;
           break;
       }
